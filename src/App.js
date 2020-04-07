2ae1f6f10db347fff36a37951be031b3350d2d72
vipinjoshi <vipiswaiting@live.com>
2020-04-07 12:52:31 +0530

    add functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import FacebookLogin from 'react-facebook-login';
 import logo from './logo.svg';
+//import TiSocialFacebookCircular from 'react-icons/lib/ti/social-facebook-circular';
+
 import './App.css';
 
+const responseFacebook = (response) => {
+  console.log(response);
+}
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <h1>LOGIN WITH FACEBOOK </h1>
+
+      <FacebookLogin
+        appId="1833903530072982" //APP ID NOT CREATED YET
+        fields="name,email,picture"
+        callback={responseFacebook}
+        cssClass="my-facebook-button-class"
+        icon="fa-facebook"
+      />
+
+    </header>
   );
 }
 
